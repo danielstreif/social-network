db4361adaad5a84e9d69cd47d6e0d026389229b6
Daniel Streif <dan.streif@gmail.com>
2021-01-13 19:25:51 +0100

    responsive styling with material-ui

--- client/src/header.js
+++ client/src/header.js
@@ -1,28 +1,39 @@
 import { Link } from "react-router-dom";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Logo from "./logo";
 import Logout from "./logout";
 
 export default function Header(props) {
+    const matches = useMediaQuery("(max-width: 720px)");
+
     return (
         <header>
             <Logo />
-            <img className="mobile-menu" src="/img/hamburgermenu.svg" alt="menu" />
-            <div className="navbar-menu">
-                <button className="header-button">
-                    <Link className="header-link" to="/users">
-                        Search
-                    </Link>
-                </button>
-                <div className="navbar-image">{props.profilePic}</div>
-                <button className="header-button">
-                    <Link className="header-link" to="/">
-                        Profile
-                    </Link>
-                </button>
-                <button className="header-button">
-                    <Logout />
-                </button>
-            </div>
+            {matches && (
+                <img
+                    className="mobile-menu"
+                    src="/img/hamburgermenu.svg"
+                    alt="menu"
+                />
+            )}
+            {!matches && (
+                <div className="navbar-menu">
+                    <button className="header-button">
+                        <Link className="header-link" to="/users">
+                            Search
+                        </Link>
+                    </button>
+                    <div className="navbar-image">{props.profilePic}</div>
+                    <button className="header-button">
+                        <Link className="header-link" to="/">
+                            Profile
+                        </Link>
+                    </button>
+                    <button className="header-button">
+                        <Logout />
+                    </button>
+                </div>
+            )}
         </header>
     );
 }
